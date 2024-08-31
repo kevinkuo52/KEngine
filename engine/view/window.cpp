8681717568ef81cdfc4af899e951bc8b439b78c4
Kevin Kuo <kevinkuo15@gmail.com>
2024-08-30 20:10:02 -0700

    full screen size

--- engine/view/window.cpp
+++ engine/view/window.cpp
@@ -14,7 +14,16 @@ Window::Window(int w, int h, std::string name)
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     //glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    window = glfwCreateWindow(w, h, name.c_str(), nullptr, nullptr);
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+    glfwWindowHint(GLFW_CENTER_CURSOR, GLFW_TRUE);// only for full screen
+
+	window = glfwCreateWindow(mode->width, mode->height, name.c_str(), nullptr, nullptr);
     glfwSetWindowUserPointer(window, this);
     glfwSetFramebufferSizeCallback(window, FramebufferResizeCallback);
 }
