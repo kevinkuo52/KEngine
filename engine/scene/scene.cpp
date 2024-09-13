82c712b22917cba5b66c49132e637f5eff7dcdc4
Kevin Kuo <kevinkuo15@gmail.com>
2024-09-12 21:25:39 -0700

    loading 2 mesh

--- engine/scene/scene.cpp
+++ engine/scene/scene.cpp
@@ -4,7 +4,11 @@ Scene::Scene(TinyObjectImporter& importer, VulkanDevice& device) : _importer(imp
 {
 	std::shared_ptr<Mesh> mesh = importer.LoadModel();
 	mesh->MeshUpload(_device);
-	_sceneNodeLookup.insert({ "level", new SceneNode("level", mesh) });
+	_sceneNodeLookup.insert({ "obj1", new SceneNode("obj1", mesh) });
+
+	std::shared_ptr<Mesh> mesh2 = importer.LoadModel();
+	mesh2->MeshUpload(_device);
+	_sceneNodeLookup.insert({ "obj2", new SceneNode("obj2", mesh2) });
 	_sceneNodeLookup.insert({ "camera", new SceneNode("camera") });
 	_sceneNodeLookup.insert({ "light", new SceneNode("light") });
 }
@@ -16,11 +20,13 @@ Scene::~Scene()
 
 void Scene::Update(float time)
 {
-	_sceneNodeLookup["level"]->SetTransformation(glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)));
+	_sceneNodeLookup["obj1"]->SetTransformation(glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)));
+	_sceneNodeLookup["obj2"]->SetTransformation(glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
 }
 
 void Scene::Draw(VkCommandBuffer commandBuffer, VkPipelineLayout pipelineLayout)
 {
 	//TODO properly traverse the graph
-	_sceneNodeLookup["level"]->Draw(commandBuffer, pipelineLayout);
+	_sceneNodeLookup["obj1"]->Draw(commandBuffer, pipelineLayout);
+	_sceneNodeLookup["obj2"]->Draw(commandBuffer, pipelineLayout);
 }
